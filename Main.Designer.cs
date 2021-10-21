b778dd5ec8c664bfea0c3d5238029d8d7a456d8f
Nhan Le <nhan.le@websparks.sg>
2021-10-21 20:06:37 +0700

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace SnapsCqq
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(103, 88);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 209);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(311, 248);
+            this.MinimumSize = new System.Drawing.Size(311, 248);
+            this.Name = "Main";
+            this.Text = "SnapsCqq";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,103 @@
+﻿using SnapsCqq.Helper;
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SnapsCqq
+{
+    public partial class Main : Form
+    {
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+
+        private const string PAGE_DOWN = "{PGDN}";
+        private const string PAGE_UP = "{PGUP}";
+        
+        private bool IsStarted = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #region Form events
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
+            timer1.Dispose();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            btnStart.Text = timer1.Enabled ? "Stop" : "Start";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            var count = rnd.Next(3, 11);
+            try
+            {
+                for (var i = 0; i < count - 2; i++)
+                {
+                    // Send key
+                    switch (rnd.Next(1, 3))
+                    {
+                        case 1:
+                            SendKeys.Send(PAGE_UP);
+                            break;
+                        case 2:
+                            SendKeys.Send(PAGE_DOWN);
+                            break;
+                    }
+                }
+            }
+            catch { }
+
+            var X = Cursor.Position.X;
+            var Y = Cursor.Position.Y;
+
+            for (var i = 0; i < count - 3; i++)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0); 
+            }
+        }
+
+        #endregion
+
+        #region System Functions
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        #endregion
+    }
+}
